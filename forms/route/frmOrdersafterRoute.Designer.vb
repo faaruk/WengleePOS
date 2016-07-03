29888b3049808cdbb1aa113125bab5ffe0eb69cc
faaruk <faaruk@yahoo.com>
2016-07-03 17:59:27 +0600

    no message

---
+++ forms/route/frmOrdersafterRoute.Designer.vb
@@ -0,0 +1,198 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmOrdersafterRoute
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.dbWengLeeDataSet = New WengLee_Application.dbWengLeeDataSet()
+        Me.tblBranchBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.tblBranchTableAdapter = New WengLee_Application.dbWengLeeDataSetTableAdapters.tblBranchTableAdapter()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DateTimePicker2 = New System.Windows.Forms.DateTimePicker()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cmbRoute = New System.Windows.Forms.ComboBox()
+        Me.cmbDriver = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.dbWengLeeDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.tblBranchBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ReportViewer1
+        '
+        Me.ReportViewer1.Location = New System.Drawing.Point(0, 30)
+        Me.ReportViewer1.Name = "ReportViewer1"
+        Me.ReportViewer1.Size = New System.Drawing.Size(1051, 510)
+        Me.ReportViewer1.TabIndex = 0
+        '
+        'dbWengLeeDataSet
+        '
+        Me.dbWengLeeDataSet.DataSetName = "dbWengLeeDataSet"
+        Me.dbWengLeeDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'tblBranchBindingSource
+        '
+        Me.tblBranchBindingSource.DataMember = "tblBranch"
+        Me.tblBranchBindingSource.DataSource = Me.dbWengLeeDataSet
+        '
+        'tblBranchTableAdapter
+        '
+        Me.tblBranchTableAdapter.ClearBeforeFill = True
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(165, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(20, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "To"
+        '
+        'DateTimePicker2
+        '
+        Me.DateTimePicker2.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.DateTimePicker2.Checked = False
+        Me.DateTimePicker2.CustomFormat = "MM/dd/yyyy"
+        Me.DateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.DateTimePicker2.Location = New System.Drawing.Point(191, 6)
+        Me.DateTimePicker2.Name = "DateTimePicker2"
+        Me.DateTimePicker2.Size = New System.Drawing.Size(112, 20)
+        Me.DateTimePicker2.TabIndex = 7
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.DateTimePicker1.Checked = False
+        Me.DateTimePicker1.CustomFormat = "MM/dd/yyyy"
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.DateTimePicker1.Location = New System.Drawing.Point(48, 6)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(112, 20)
+        Me.DateTimePicker1.TabIndex = 6
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Button2.Location = New System.Drawing.Point(716, 3)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(129, 27)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Show"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 10)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(36, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "From: "
+        '
+        'cmbRoute
+        '
+        Me.cmbRoute.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.cmbRoute.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.cmbRoute.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.cmbRoute.FormattingEnabled = True
+        Me.cmbRoute.Location = New System.Drawing.Point(551, 6)
+        Me.cmbRoute.MaximumSize = New System.Drawing.Size(370, 0)
+        Me.cmbRoute.Name = "cmbRoute"
+        Me.cmbRoute.Size = New System.Drawing.Size(159, 21)
+        Me.cmbRoute.TabIndex = 10
+        '
+        'cmbDriver
+        '
+        Me.cmbDriver.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.cmbDriver.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.cmbDriver.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.cmbDriver.FormattingEnabled = True
+        Me.cmbDriver.Location = New System.Drawing.Point(353, 6)
+        Me.cmbDriver.MaximumSize = New System.Drawing.Size(370, 0)
+        Me.cmbDriver.Name = "cmbDriver"
+        Me.cmbDriver.Size = New System.Drawing.Size(150, 21)
+        Me.cmbDriver.TabIndex = 11
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(507, 10)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Route: "
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(311, 10)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Driver: "
+        '
+        'frmOrdersafterRoute
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1051, 510)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.cmbDriver)
+        Me.Controls.Add(Me.cmbRoute)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DateTimePicker2)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ReportViewer1)
+        Me.Name = "frmOrdersafterRoute"
+        Me.Text = "Late Orders"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.dbWengLeeDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.tblBranchBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+    Friend WithEvents tblBranchBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents dbWengLeeDataSet As WengLee_Application.dbWengLeeDataSet
+    Friend WithEvents tblBranchTableAdapter As WengLee_Application.dbWengLeeDataSetTableAdapters.tblBranchTableAdapter
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents DateTimePicker2 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents cmbRoute As System.Windows.Forms.ComboBox
+    Friend WithEvents cmbDriver As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ forms/route/frmOrdersafterRoute.vb
@@ -0,0 +1,64 @@
+﻿Public Class frmOrdersafterRoute
+    Public RouteId As String
+    Private Sub ReportViewer1_Load(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'dbWengLeeDataSet.tblBranch' table. You can move, or remove it, as needed.
+        ReportViewer1.Width = Width - 30
+        ReportViewer1.Height = Height - 40
+        FillCombo(cmbRoute, "OtherInfos")
+        FillCombo(cmbDriver, "Driver")
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Dim dt As New DataTable
+        Dim dtsummary As New DataTable
+        Dim objOrder As New cls_tblOrder
+        dt = objOrder.SelectOrdersAfterRoute(RouteId)
+
+        ReportViewer1.Reset()
+
+        ReportViewer1.LocalReport.ReportEmbeddedResource = "WengLee_Application.OrdersCreatedAfterRoute.rdlc"
+        ReportViewer1.LocalReport.DataSources.Clear()
+        ReportViewer1.LocalReport.DataSources.Add(New Microsoft.Reporting.WinForms.ReportDataSource("DataSet1", dt))
+        ReportViewer1.ZoomPercent = 100
+        ReportViewer1.SetDisplayMode(Microsoft.Reporting.WinForms.DisplayMode.PrintLayout)
+        ReportViewer1.ZoomMode = Microsoft.Reporting.WinForms.ZoomMode.Percent
+        ReportViewer1.ZoomPercent = 100
+        ReportViewer1.RefreshReport()
+    End Sub
+    Private Sub FillCombo(ByVal cmb As ComboBox, ByVal _type As String)
+        Dim FirstItemText As String = "<<All>>"
+        Dim objConn As New clsConnection
+        Dim conn As SqlConnection = objConn.connect
+        Dim strSQL As String
+        strSQL = "SELECT distinct " + _type + " as Display, " + _type + " as Value "
+        strSQL += "FROM tblRoute "
+        strSQL += "where RouteDate>=convert(datetime, '" + DateTimePicker1.Value.Date.ToString() + "', 101) "
+        strSQL += "and RouteDate<=convert(datetime, '" + DateTimePicker2.Value.Date.ToString() + "', 101) "
+        strSQL += "order by " + _type + " "
+        Dim da As New SqlDataAdapter(strSQL, conn)
+        Dim dt As New DataTable
+        da.Fill(dt)
+        Dim dr As DataRow = dt.NewRow
+        dr(0) = FirstItemText
+        dr(1) = "0"
+        dt.Rows.InsertAt(dr, 0)
+        cmb.DisplayMember = "Display"
+        cmb.ValueMember = "Value"
+        cmb.DataSource = dt
+    End Sub
+
+    Private Sub DateTimePicker2_ValueChanged(sender As System.Object, e As System.EventArgs) Handles DateTimePicker2.ValueChanged
+        FillCombo(cmbRoute, "OtherInfos")
+        FillCombo(cmbDriver, "Driver")
+    End Sub
+
+    Private Sub DateTimePicker1_ValueChanged(sender As System.Object, e As System.EventArgs) Handles DateTimePicker1.ValueChanged
+        FillCombo(cmbRoute, "OtherInfos")
+        FillCombo(cmbDriver, "Driver")
+    End Sub
+
+End Class
